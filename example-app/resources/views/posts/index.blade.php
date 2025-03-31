492dbde85209db504b28141e407eb10202e1b8c9
sheviConstanz <shevi.1720@gmail.com>
2025-03-31 15:55:45 -0300

    crud y navbar
    crud y navbar con mismo formato, se utilizo la funcion @extend @yield

--- example-app/resources/views/posts/index.blade.php
+++ example-app/resources/views/posts/index.blade.php
@@ -1,27 +1,9 @@
-<div>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
-integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-  <title>Posts</title>
-</head>
-<body>
-  <nav class="navbar navbar-expand-lg navbar-light bg-warning">
-    <div class="container-fluid">
-      <a class="navbar-brand h1" href={{ route('posts.index') }}>CRUDPosts</a>
-      <div class="justify-end ">
-        <div class="col ">
-          <a class="btn btn-sm btn-success" href={{ route('posts.create') }}>Add Post</a>
-        </div>
-      </div>
-    </div>
-  </nav>
+@extends('layouts.app')
+
+@section('content')
   <div class="container mt-5">
     <div class="row">
+          <a class="btn btn-sm btn-success" href={{ route('posts.create') }}>Add Post</a>
       @foreach ($posts as $post)
         <div class="col-sm">
           <div class="card">
@@ -51,6 +33,4 @@ integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6
       @endforeach
     </div>
   </div>
-</body>
-</html>
-</div>
+@endsection
