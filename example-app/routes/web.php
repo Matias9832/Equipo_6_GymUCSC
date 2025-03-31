2e0d7abed6bb4b41697cbc6617646c6eb9a7c6f3
Matías Carrasco <mcarrascoa@ing.ucsc.cl>
2025-03-31 15:52:21 -0300

    Ajuste NAV y CRUD

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -19,21 +19,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// returns the home page with all posts
-Route::get('/crud', PostController::class .'@index')->name('posts.index');
-// returns the form for adding a post
-Route::get('/posts/create', PostController::class . '@create')->name('posts.create');
-// adds a post to the database
-Route::post('/posts', PostController::class .'@store')->name('posts.store');
-// returns a page that shows a full post
-Route::get('/posts/{post}', PostController::class .'@show')->name('posts.show');
-// returns the form for editing a post
-Route::get('/posts/{post}/edit', PostController::class .'@edit')->name('posts.edit');
-// updates a post
-Route::put('/posts/{post}', PostController::class .'@update')->name('posts.update');
-// deletes a post
-Route::delete('/posts/{post}', PostController::class .'@destroy')->name('posts.destroy');
-
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -42,6 +27,22 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // returns the home page with all posts
+    Route::get('/crud', PostController::class . '@index')->name('posts.index');
+    // returns the form for adding a post
+    Route::get('/posts/create', PostController::class . '@create')->name('posts.create');
+    // adds a post to the database
+    Route::post('/posts', PostController::class . '@store')->name('posts.store');
+    // returns a page that shows a full post
+    Route::get('/posts/{post}', PostController::class . '@show')->name('posts.show');
+    // returns the form for editing a post
+    Route::get('/posts/{post}/edit', PostController::class . '@edit')->name('posts.edit');
+    // updates a post
+    Route::put('/posts/{post}', PostController::class . '@update')->name('posts.update');
+    // deletes a post
+    Route::delete('/posts/{post}', PostController::class . '@destroy')->name('posts.destroy');
+
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
