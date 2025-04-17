0f021e9e2d0459338f2cd1e68b5460324c15ae53
MordekaiBlack <jguzmana@ing.ucsc.cl>
2025-04-17 00:03:02 -0400

    Creación inicial de página de mantenedores
    todavía me falta administrar el acceso que sea solo para admin (eso se ve en routes, middleware) y empezar a modificar tablas con la página ya creada.

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -23,6 +23,10 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/maintainers', function () {
+    return view('maintainers');
+})->middleware(['auth'])->name('maintainers');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
