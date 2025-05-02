c76a59180b90817b07d7497c0d1a6185327765f4
MatiasCarrasco9832 <mcarrascoa@ing.ucsc.cl>
2025-05-01 23:08:07 -0400

    Migration usuario y administrador

--- desarrollo-ucsc/database/migrations/2025_04_18_225310_create_administrador_table.php
+++ desarrollo-ucsc/database/migrations/2025_04_18_225310_create_administrador_table.php
@@ -14,6 +14,9 @@ return new class extends Migration
         Schema::create('administrador', function (Blueprint $table) {
             $table->id('id_admin');
 
+            $table->string('rut_admin');
+            $table->foreign('rut_admin')->references('rut')->on('usuario');
+
             $table->string('nombre_admin');
             $table->timestamp('fecha_creacion');
         });
