1aac887aba958a77bda0c9dc55b0d1a1a1356310
MordekaiBlack <jguzmana@ing.ucsc.cl>
2025-05-11 15:39:19 -0400

    Fix Eliminación de admin
    Fix: Eliminación de usuarios administradores

--- desarrollo-ucsc/database/migrations/2025_04_18_225410_create_admin_sucursal_table.php
+++ desarrollo-ucsc/database/migrations/2025_04_18_225410_create_admin_sucursal_table.php
@@ -6,26 +6,26 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('admin_sucursal', function (Blueprint $table) {
             $table->unsignedBigInteger('id_suc');
             $table->unsignedBigInteger('id_admin');
             $table->primary(['id_suc', 'id_admin']);
-
+    
             $table->boolean('activa');
-
-            $table->foreign('id_suc')->references('id_suc')->on('sucursal');
-            $table->foreign('id_admin')->references('id_admin')->on('administrador');
+    
+            $table->foreign('id_suc')
+                  ->references('id_suc')
+                  ->on('sucursal')
+                  ->onDelete('cascade'); // Si se elimina la sucursal, elimina esta relación
+    
+            $table->foreign('id_admin')
+                  ->references('id_admin')
+                  ->on('administrador')
+                  ->onDelete('cascade'); // Si se elimina el administrador, elimina esta relación
         });
     }
-
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('admin_sucursal');
