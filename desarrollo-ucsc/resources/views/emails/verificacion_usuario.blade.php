2ab24f6350abaaf2cf5c2715f95e4304fd8f7a9f
MordekaiBlack <jguzmana@ing.ucsc.cl>
2025-05-11 23:04:07 -0400

    Fix verificación de usuario

--- desarrollo-ucsc/resources/views/emails/verificacion_usuario.blade.php
+++ desarrollo-ucsc/resources/views/emails/verificacion_usuario.blade.php
@@ -6,7 +6,7 @@
     <title>Verificación de Cuenta</title>
 </head>
 <body>
-    <h1>Hola {{ $rut }}!</h1>
+    <h1>Hola {{ $nombreCompleto }}!</h1>
     <p>Gracias por registrarte. Por favor, usa el siguiente código para verificar tu cuenta:</p>
     <h2 style="color: red;">{{ $codigo }}</h2>
     <p>Si no realizaste esta solicitud, ignora este correo.</p>
