c50a494eff146bb8dbdf6c8ef5b144934080b2ba
sheviConstanz <shevi.1720@gmail.com>
2025-05-28 00:12:53 -0400

    Seeder ejercicios
    Se sube el seeder de ejercicios mas las imagenes

--- desarrollo-ucsc/database/seeders/DatabaseSeeder.php
+++ desarrollo-ucsc/database/seeders/DatabaseSeeder.php
@@ -45,6 +45,9 @@ class DatabaseSeeder extends Seeder
             TallerSeeder::class,
         ]);
 
+        //Seeder de los Ejercicios
+        $this->call(EjercicioSeeder::class);
+
         $this->call(FontColorSeeder::class);
         $this->call(TemaSeeder::class);
         $this->call(TenantSeeder::class);
