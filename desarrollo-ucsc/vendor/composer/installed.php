972fcb1e5c54b5acde8d104859e93986176f868c
MordekaiBlack <jguzmana@ing.ucsc.cl>
2025-06-05 00:03:31 -0400

    git ignore actualizado

--- desarrollo-ucsc/vendor/composer/installed.php
+++ desarrollo-ucsc/vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '5020de8f4980a150cff5fea39bb5a8c6369fce6b',
+        'reference' => 'a73b02a21c4ff11847928270179ba5581f615a28',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -445,7 +445,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '5020de8f4980a150cff5fea39bb5a8c6369fce6b',
+            'reference' => 'a73b02a21c4ff11847928270179ba5581f615a28',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
@@ -883,8 +883,8 @@
         'psr/log-implementation' => array(
             'dev_requirement' => false,
             'provided' => array(
-                0 => '1.0|2.0|3.0',
-                1 => '3.0.0',
+                0 => '3.0.0',
+                1 => '1.0|2.0|3.0',
             ),
         ),
         'psr/simple-cache' => array(
