ad644bc6ff016734cb02fdf90b66e23cd0e434b1
MatiasCarrasco9832 <mcarrascoa@ing.ucsc.cl>
2025-06-10 23:14:29 -0400

    Eliminación Planes tenants

--- desarrollo-ucsc/routes/web.php
+++ desarrollo-ucsc/routes/web.php
@@ -6,9 +6,6 @@ use App\Http\Controllers\Tenants\TenantController;
 use App\Http\Controllers\Tenants\Personalizacion\TemaController;
 use App\Http\Controllers\Tenants\Personalizacion\ColorController;
 use App\Http\Controllers\Tenants\Personalizacion\FuenteController;
-use App\Http\Controllers\Tenants\Plan\PlanController;
-use App\Http\Controllers\Tenants\Plan\CuentaController;
-use App\Http\Controllers\Tenants\Plan\PermisoController;
 
 Route::get('/start', function () {
     return view('start');
@@ -21,11 +18,5 @@ Route::prefix('personalizacion')->name('personalizacion.')->group(function () {
     Route::resource('temas', TemaController::class);
     Route::resource('colores', ColorController::class);
     Route::resource('fuentes', FuenteController::class);
-});
-
-Route::prefix('plan')->name('plan.')->group(function () {
-    Route::get('planes', [PlanController::class, 'index'])->name('planes.index');
-    Route::get('cuentas', [CuentaController::class, 'index'])->name('cuentas.index');
-    Route::get('permisos', [PermisoController::class, 'index'])->name('permisos.index');
 });
 
