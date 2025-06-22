07200d59e2546984622648717cb666d6d0711bde
MordekaiBlack <jguzmana@ing.ucsc.cl>
2025-06-22 18:13:42 -0400

    Fix: Botón de configuración de mi perfil

--- desarrollo-ucsc/resources/views/layouts/app.blade.php
+++ desarrollo-ucsc/resources/views/layouts/app.blade.php
@@ -39,7 +39,6 @@
     <script src="https://code.jquery.com/jquery-3.7.0.min.js"></script>
     <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap4.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
     @php
         use App\Models\Tenants\TemaTenant;
